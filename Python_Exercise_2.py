fd57206f738505783d5821656988ccf8812734be
Sayan Ghosh <70215356+NightWalker42o0p@users.noreply.github.com>
2024-02-27 16:46:06 +0530

    Update Python_Exercise_2.py
    Corrected Logic

--- Python_Exercise_2.py
+++ Python_Exercise_2.py
@@ -2,6 +2,8 @@ print("Welcome To Tip Generator")
 bill=float(input("What was the amount of the bill?"))
 tip=float(input("What percentage of tip would you like to give?"))
 people=int(input("How many people to split the bill?"))
-tip_num=(bill+(tip/100))/people
+tip_num=(tip/100)*bill
 tip_amount=round(tip_num,2)
-print(f"Each person should pay {tip_amount}")
+total_bill=(bill+tip_amount)
+perhead_pay=round(total_bill/people,2)
+print(f"Each person should pay {perhead_pay}")
